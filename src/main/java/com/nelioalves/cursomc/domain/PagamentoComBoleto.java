7027f1b1f14653c3b8c9b327dbf1956253eeda63
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2019-09-05 14:05:58 -0300

    Atualizando somente JsonIgnore

--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -3,8 +3,6 @@ package com.nelioalves.cursomc.domain;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
@@ -26,4 +24,21 @@ public class PagamentoComBoleto extends Pagamento {
 		this.dataVencimento = dataVencimento;
 		this.dataPagamento = dataPagamento;
 	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
 }
